c65b7b568a01e8f41cb0ec2eaa35ee76a110f378
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-12-28 18:59:47 +0300

    JS Learn

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ title.innerHTML = "Hello JavaScript!";
 // Interacting with the DOM using the querySelector method
 const test = document.querySelector("h3");
 test.innerHTML = "The DOM is awesome!";
+
+function showValue(number = 1) {
+  const value = document.querySelector("p");
+  number = parseInt(number);
+  value.innerHTML = `The DOM value is  ${number + 1}`;
+}
