7bc7a33f1b28447631b30cdabbd06d37ad9f50f3
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-12-28 22:12:38 +0300

    JS Learn

---
+++ script.js
@@ -0,0 +1,42 @@
+const fullName = "Joseph Mbote";
+
+var course = "JavaScript for Beginners";
+console.log(`Hello ${fullName}!`);
+var isLoggedIn = true;
+var loggedCount = 34;
+
+var title = document.querySelector("h1");
+title.innerHTML = course;
+
+var subscribe = document.querySelector("button");
+if (isLoggedIn) {
+  subscribe.textContent = "Unsubscribe";
+} else {
+  subscribe.textContent = "Subscribe";
+}
+
+function showValue() {
+  isLoggedIn = !isLoggedIn;
+  if (isLoggedIn) {
+    subscribe.textContent = "Unsubscribe";
+  } else {
+    subscribe.textContent = "Subscribe";
+  }
+}
+
+var email = "mbotejoseph001@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+
+var courseCount = 0;
+
+console.log(`
+    With Unique ID: ${email}
+    User password: ${password}
+    User confirmed password: ${confirmPassword}
+    User is logged in: ${isLoggedIn}
+    User has logged in ${loggedCount} times
+    User is subscribed to ${courseCount} courses
+`);
+
+console.log(document.body);
