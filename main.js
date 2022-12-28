7eb40db6c2ae41e8ef58beaece23eca3bf657ccb
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-12-28 13:26:43 +0300

    JS Learn

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Hello JavaScript!");
+
+// Interacting with the DOM
+const title = document.getElementsByClassName("title")[0];
+title.innerHTML = "Hello JavaScript!";
+
+// Interacting with the DOM using the querySelector method
+const test = document.querySelector("h3");
+test.innerHTML = "The DOM is awesome!";
