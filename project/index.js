99cf752c85b7de18852eee93b3df286ee60619f1
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-01-04 23:19:40 +0300

    Scrimba Courses Learning

--- project/index.js
+++ project/index.js
@@ -10,23 +10,33 @@ let Courses = [
   { name: "Django", price: 1000 },
 ];
 
-(() => {
-  function generateList() {
-    const ul = document.querySelector(".list-group");
-    ul.innerHTML = "";
-    Courses.forEach((course) => {
-      const li = document.createElement("li");
-      li.classList.add("list-group-item");
-      const name = document.createTextNode(course.name);
-      li.appendChild(name);
-      ul.appendChild(li);
+function generateList() {
+  const ul = document.querySelector(".list-group");
+  ul.innerHTML = "";
+  Courses.forEach((course) => {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    const name = document.createTextNode(course.name);
+    li.appendChild(name);
+    ul.appendChild(li);
 
-      const span = document.createElement("span");
-      span.classList.add("float-right");
-      const price = document.createTextNode("$" + course.price);
-      span.appendChild(price);
-      li.appendChild(span);
-    });
-  }
+    const span = document.createElement("span");
+    span.classList.add("float-right");
+    const price = document.createTextNode("$" + course.price);
+    span.appendChild(price);
+    li.appendChild(span);
+  });
+}
+generateList();
+
+// sort Courses by price in ascending order
+function sortCourses() {
+  Courses.sort((a, b) => a.price - b.price);
   generateList();
-})();
+}
+
+// sort Courses by price in descending order
+function sortCoursesDesc() {
+  Courses.sort((a, b) => b.price - a.price);
+  generateList();
+}
